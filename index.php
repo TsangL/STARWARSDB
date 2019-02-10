1480f51a4df083eba5bc768a293ea87429dcf332
Ryanwall15 <wallerir@oregonstate.edu>
2019-02-10 14:32:43 -0800

    Test changes

--- index.php
+++ index.php
@@ -2,29 +2,29 @@
 <!-- simple.html -->
 
 <html lang="en">
-    
+
     <head>
-		
-		
+
+
         <meta charset="utf-8">
         <title>Cookie Cutter</title>
         <link rel="stylesheet" href="CSS/CSSStuff.css">
-        
+
         <style type="text/css">
-           
+
         </style>
-        
+
     </head>
         <div id="picture1">
             <img src= "og-generic_02031d2b.png" alt="Starwars" width="100" height="100">
         </div>
-        
+
     <h2>
         <br>
-		
-        <div id="bigspace">Welcome to Cookie Cutter.com</div>
-        
-        <div id="italictxt"> "Cookie Cutter.com is dedicated to our customers unique cookie cutting needs."  </div>
+
+        <div id="bigspace">Star Wars Database</div>
+
+        <div id="italictxt"> "This is group 26's project for CS 340" </div>
     </h2>
         <br>
     <hr>
@@ -33,21 +33,21 @@
         <div id="menulist">
 		<br>
 		<br>
-		
+
 				<ul>
-					<li class="medium1"> Site Menu: </li>  
+					<li class="medium1"> Site Menu: </li>
 					<br>
-					<li class="small1"><a href="index.php"> Home </a></li> 
+					<li class="small1"><a href="index.php"> Home </a></li>
 					<br>
-					<li class="small1"><a href="characters.php"> StarWars Characters </a></li> 
+					<li class="small1"><a href="characters.php"> StarWars Characters </a></li>
 					<br>
-					<li class="small1"><a href="planets.php"> StarWars Planets </a></li> 
+					<li class="small1"><a href="planets.php"> StarWars Planets </a></li>
 					<br>
 					<li class="small1"><a href="ships.php"> StarWars Ships </a></li>
 					<br>
 					<li class="small1"><a href="weapons.php"> StarWars Weapons </a></li>
-					
-				
+
+
 				</ul>
         </div>
         <div id="box">
@@ -65,11 +65,7 @@
         <br>
         <br>
         <hr>
-			
-		
+
+
     </body>
 <html>
-    
-    
-    
-    
